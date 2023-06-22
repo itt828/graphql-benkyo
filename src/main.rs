dcc76d5f952cb5109f5c860ce0f46143f1e10392
itt828 <iittt828@gmail.com>
2023-06-22 10:48:47 +0900

    :sparkles: Add graphql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::*;
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let res = schema.execute("{add(a: 19, b:22)}").await;
+    let json = serde_json::to_string(&res);
+    println!("{:#?}", json);
 }
