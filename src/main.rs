760a18f9baaa0256f7bc1fac0eaf0d8390dc493b
itt828 <iittt828@gmail.com>
2023-06-23 01:42:15 +0900

    :sparkles: Impl first app

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,74 @@
+use async_graphql::{http::GraphiQLSource, *};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Extension, Router,
+};
 use std::net::SocketAddr;
+use uuid::Uuid;
 
-use async_graphql::*;
-use axum::{routing, Router};
+#[derive(SimpleObject)]
+struct User {
+    id: ID,
+    name: String,
+}
+
+#[derive(SimpleObject)]
+struct Tag {
+    id: ID,
+    name: String,
+}
+
+#[derive(SimpleObject)]
+struct Blog {
+    id: ID,
+    title: String,
+    tags: Vec<Tag>,
+    content: String,
+}
 
 struct Query;
-
 #[Object]
 impl Query {
-    async fn add(&self, a: i32, b: i32) -> i32 {
-        a + b
+    async fn user(&self) -> anyhow::Result<Option<User>> {
+        println!("user query called");
+        Ok(Some(User {
+            id: ID(Uuid::new_v4().to_string()),
+            name: String::from("itt"),
+        }))
     }
+    async fn tag(&self) -> anyhow::Result<Option<Tag>> {
+        Ok(Some(Tag {
+            id: ID(Uuid::new_v4().to_string()),
+            name: String::from("tag"),
+        }))
+    }
+}
+type GQLSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+async fn graphql_handler(schema: Extension<GQLSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    println!("handler");
+    schema.execute(req.into_inner()).await.into()
+}
+async fn graphiql() -> impl IntoResponse {
+    println!("graphiql");
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
 }
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", routing::get(|| async { "oisu:" }));
+async fn main() -> anyhow::Result<()> {
+    let schema: GQLSchema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    println!("schema built!");
+    println!("{}", &schema.sdl());
+    let app = Router::new()
+        .route("/", get(graphiql).post(graphql_handler))
+        .layer(Extension(schema));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening at 3000");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+    Ok(())
 }
