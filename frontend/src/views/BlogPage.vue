a22e2085349b509786ea23212c5c57571c097306
itt828 <iittt828@gmail.com>
2023-06-30 02:58:47 +0900

    :sparkles: Update css

---
+++ frontend/src/views/BlogPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>hoge</div>
+  <div>hoge</div>
+  <div>hoge</div>
+  <div>hoge</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const blog = ref()
+</script>
