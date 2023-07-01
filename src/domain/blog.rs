cb55062f652c51d3c6688ab69ac72be59c5a840e
itt828 <iittt828@gmail.com>
2023-07-02 03:12:51 +0900

    :zap: Impl blog domain new

--- src/domain/blog.rs
+++ src/domain/blog.rs
@@ -9,3 +9,15 @@ pub struct Blog {
     pub authors: Vec<User>,
     pub tags: Vec<Tag>,
 }
+
+impl Blog {
+    pub fn new(id: Uuid, title: &str, content: &str) -> Self {
+        Self {
+            id,
+            title: title.to_string(),
+            content: content.to_string(),
+            authors: vec![],
+            tags: vec![],
+        }
+    }
+}
