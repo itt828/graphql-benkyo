0fc5e62eb12a757dfb743cdec9736ebf4c1bc732
itt828 <iittt828@gmail.com>
2023-07-02 15:03:16 +0900

    :zap: Fix domains

--- src/domain/tag.rs
+++ src/domain/tag.rs
@@ -1,6 +1,32 @@
 use uuid::Uuid;
 
+use super::error::BlogError;
+
 pub struct Tag {
-    id: Uuid,
-    name: String,
+    pub id: Uuid,
+    pub name: String,
+}
+
+impl Tag {
+    pub fn new(id: Uuid, name: &str) -> Self {
+        Self {
+            id,
+            name: name.to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TagName(String);
+
+impl TagName {
+    pub fn new(s: &str) -> Result<Self, BlogError> {
+        if s.chars().count() < 20 {
+            Ok(TagName(s.to_string()))
+        } else {
+            Err(BlogError::ValidationError(
+                "タグの長さは20文字以下にしてください。".to_string(),
+            ))
+        }
+    }
 }
--- src/domain/user.rs
+++ src/domain/user.rs
@@ -1,6 +1,23 @@
 use uuid::Uuid;
 
+use super::error::BlogError;
+
 pub struct User {
     pub id: Uuid,
     pub name: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct UserName(String);
+
+impl UserName {
+    pub fn new(s: &str) -> Result<Self, BlogError> {
+        if s.chars().count() < 20 {
+            Ok(UserName(s.to_string()))
+        } else {
+            Err(BlogError::ValidationError(
+                "ユーザー名は20文字以下にしてください。".to_string(),
+            ))
+        }
+    }
 }
