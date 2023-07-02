e1b054a512c1f93fa0caffc477c333cab2de13d1
itt828 <iittt828@gmail.com>
2023-07-02 17:53:39 +0900

    :fire: Remove

--- src/handler/graphql/tag.rs
+++
@@ -1,7 +0,0 @@
-use async_graphql::{SimpleObject, ID};
-
-#[derive(SimpleObject)]
-pub struct Tag {
-    id: ID,
-    name: String,
-}
--- src/handler/graphql/user.rs
+++
@@ -1,7 +0,0 @@
-use async_graphql::{SimpleObject, ID};
-
-#[derive(SimpleObject)]
-struct User {
-    id: ID,
-    name: String,
-}
