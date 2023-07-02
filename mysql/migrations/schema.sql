d6ee5653fc430c191946f193a547d92106d7521c
itt828 <iittt828@gmail.com>
2023-07-02 18:33:21 +0900

    :adhesive_bandage: Fix

--- mysql/migrations/schema.sql
+++ mysql/migrations/schema.sql
@@ -4,5 +4,5 @@ create table if not exists `blog` (
     `id` CHAR(36) not null primary key,
     `title` text not null,
     `content` text not null,
-    `created_at` datetime  not null default current_timestamp
+    `created_at` datetime not null default current_timestamp
 );
